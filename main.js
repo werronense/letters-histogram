0d6897f60cd55048e16728cd5d88dadc6951c241
werronense <31091965+werronense@users.noreply.github.com>
2019-07-23 16:50:53 -0400

    Refine algorithm to produce data for join

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // create data object and initialize it by looping char codes for
+  // all lower case letters and setting starting frequency to 0
+  const data = {};
+  for (i = 97; i <= 122; i++) {
+    data[String.fromCharCode(i)] = 0;
+  }
+
   const input = document.querySelector("#input");
   const display = d3.select("#display");
   const chart = display.append("svg");
@@ -27,9 +34,13 @@ document.addEventListener("DOMContentLoaded", () => {
   input.addEventListener("keyup", e => {
     const letters = makeLettersArray(e.target.textContent);
     const frequencyHistogram = convertArrayToHistogram(letters);
+    const keysOfHistogram = Object.keys(frequencyHistogram);
 
-    console.log(frequencyHistogram); // test
-
+    keysOfHistogram.forEach(key => {
+      if (data[key] !== frequencyHistogram[key]) {
+        data[key] = frequencyHistogram[key];
+      }
+    })
   });
 
 
