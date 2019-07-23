45e2dc96255c31fcfb1d092424c6b4322cc4bfd3
werronense <31091965+werronense@users.noreply.github.com>
2019-07-23 12:27:11 -0400

    Refactor histogram algorithm

--- main.js
+++ main.js
@@ -1,20 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
   const input = document.querySelector(".input");
 
-  input.addEventListener("keyup", e => {
-    const histogram = {};
 
-    const letters = e.target.textContent.toLowerCase().split("").filter(
+  // define functions
+  function makeLettersArray(string) {
+    return string.toLowerCase().split("").filter(
         letter => /[a-z]/.test(letter)
     );
+  }
 
-    letters.forEach(letter => {
+  function convertArrayToHistogram(array) {
+    const histogram = {}
+
+    array.forEach(letter => {
       return (histogram[letter] ? histogram[letter] += 1 :
           histogram[letter] = 1)
     });
 
-    console.log(histogram);
+    return histogram;
+  }
+
+
+  // add event listener to input field
+  input.addEventListener("keyup", e => {
+    const letters = makeLettersArray(e.target.textContent);
+    const frequencyHistogram = convertArrayToHistogram(letters);
+
+    console.log(frequencyHistogram);
   });
 
+  
   input.focus();
 });
