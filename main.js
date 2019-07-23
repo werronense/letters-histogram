c079203b8785f748e66d986fbafb46130f505805
werronense <31091965+werronense@users.noreply.github.com>
2019-07-23 12:17:20 -0400

    Create basic histogram algorithm

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   const input = document.querySelector(".input");
 
+  input.addEventListener("keyup", e => {
+    const histogram = {};
+
+    const letters = e.target.textContent.toLowerCase().split("").filter(
+        letter => /[a-z]/.test(letter)
+    );
+
+    letters.forEach(letter => {
+      return (histogram[letter] ? histogram[letter] += 1 :
+          histogram[letter] = 1)
+    });
+
+    console.log(histogram);
+  });
+
   input.focus();
 });
