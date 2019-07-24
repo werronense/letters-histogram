6a1fd7ed84319811fdccd8b59401e8fd2a2b5c30
werronense <31091965+werronense@users.noreply.github.com>
2019-07-24 06:49:08 -0400

    Solve data structure issue
    Defined a getter method on the main data object to return the
    frequencies in the correct alphabetical order.

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   // create data object and initialize it by looping char codes for
   // all lower case letters and setting starting frequency to 0
-  const data = {};
+  const data = {
+    // define a getter method to return the histogram values in
+    // alphabetical order
+    get frequencies() {
+      const orderedValues = [];
+      for (i = 97; i <= 122; i++) {
+        orderedValues.push(data[String.fromCharCode(i)]);
+      }
+      return orderedValues;
+    }
+  };
+
   for (i = 97; i <= 122; i++) {
     data[String.fromCharCode(i)] = 0;
   }
 
   const input = document.querySelector("#input");
+
+  // d3 code for the histogram
   const display = d3.select("#display");
   const chart = display.append("svg");
-
+  const bar = chart.selectAll("g")
+      .data(Object.values(data)) // current solution, resume here
+      .enter().append("g");
 
   // define functions
   function makeLettersArray(string) {
@@ -41,8 +56,11 @@ document.addEventListener("DOMContentLoaded", () => {
         data[key] = frequencyHistogram[key];
       }
     })
+    console.log(data.frequencies);
   });
 
 
   input.focus();
+  console.log("running all"); // test
+  //console.log(data.frequencies); // test
 });
